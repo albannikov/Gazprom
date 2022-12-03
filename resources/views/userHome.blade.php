cd9c55ee79772a92d40baf8ff9d04a5e7e96c460
audiowave33 <martinenkova@admrad.ru>
2022-12-03 13:04:48 +0300

    Выводит список значений машин

--- resources/views/userHome.blade.php
+++ resources/views/userHome.blade.php
@@ -6,4 +6,16 @@
 @foreach($cars as $car)
      <p>{{ $car->state_number }}</p>
      <p>{{ $car->model }}</p>
+     <p>{{ $car->year_of_release }}</p>
+     <p>{{ $car->code_ts }}</p>
+     <p>{{ $car->type_of_transport_service }}</p>
+     <p>{{ $car->view_ts }}</p>
+     <p>{{ $car->type_ts }}</p>
+     <p>{{ $car->the_value_of_the_vehicle_type_characteristic }}</p>
+     <p>{{ $car->country_of_manufacture }}</p>
+     <p>{{ $car->type_of_fuel }}</p>
+     <p>{{ $car->supplier }}</p>
+     <p>{{ $car->subcontracting }}</p>
+     <p>{{ $car->the_basis_of_ownership }}</p>
+
 @endforeach
