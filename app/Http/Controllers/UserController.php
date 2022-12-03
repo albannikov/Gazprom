166929f259b5db2e7d14b618a2686fa8eb7cb780
audiowave33 <martinenkova@admrad.ru>
2022-12-03 14:12:43 +0300

    Изменил возможность добавлять машину

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-
+use App\Models\Contractors;
 use App\Models\User;
 use App\Models\Car;
 use Auth;
@@ -14,11 +14,12 @@ class UserController extends Controller
     public function addCar(Request $request)
     {
         $id = Auth::id();
+        $supplier = Contractors::where('id_user',$id)->first('organization_name');
+        
         Car::create([
             'id_user' => $id,
             'state_number' => $request->input('state_number'),
             'model' => $request->input('model'),
-
             'year_of_release' => $request->input('year_of_release'),
             'code_ts' => $request->input('code_ts'),
             'type_of_transport_service' => $request->input('type_of_transport_service'),
@@ -27,7 +28,7 @@ class UserController extends Controller
             'the_value_of_the_vehicle_type_characteristic' => $request->input('the_value_of_the_vehicle_type_characteristic'),
             'country_of_manufacture' => $request->input('country_of_manufacture'),
             'type_of_fuel' => $request->input('type_of_fuel'),
-            'supplier' => $request->input('supplier'),
+            'supplier' => $supplier->organization_name,
             'subcontracting' => $request->input('subcontracting'),
             'the_basis_of_ownership' => $request->input('the_basis_of_ownership'),
             
