64a778a945829332ac1381d90a6d8ce401de1f94
audiowave33 <martinenkova@admrad.ru>
2022-12-04 01:00:25 +0300

    Добавил бланк

--- app/Http/Controllers/QRController.php
+++ app/Http/Controllers/QRController.php
@@ -5,14 +5,18 @@ namespace App\Http\Controllers;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Pass;
+use App\Models\
 use QRCode;
 
 class QRController extends Controller
 {
     public function index(Request $request,$number){
         #QrCode::encoding('UTF-8')->size(500)->generate('Добро пожаловать на jobtools.ru');
+
+        
         if(Pass::where('qr_code', $number)->first()){;
-            $info = Pass::where('qr_code',$number)->first();
+            #$info = Pass::where('qr_code',$number)->first();
+
             return view('info',compact('info'));
         }
         else{
