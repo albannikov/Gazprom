56a354ea604cfc3c286c11b9f9ae01785d20d8d4
audiowave33 <martinenkova@admrad.ru>
2022-12-03 22:41:13 +0300

    Добавил номер машины в заявке

--- resources/views/application.blade.php
+++ resources/views/application.blade.php
@@ -75,7 +75,12 @@
 
       <form action="{{ url('/add-application') }}" method="POST">
      @csrf
-     <p>Номер машины<input type="text" name="state_number"></p>
+     <p>Номер машины </p>
+     <select name="state_number">
+        @foreach($cars as $car)
+          <option>{{ $car->state_number }}</option>
+        @endforeach
+    </select>
      <p>Точка А<input type="text" name="point_a"></p>
      <p>Точка Б<input type="text" name="point_b"></p>
      <p>Срок действия с<input type="date" name="period_from">по<input type="date" name="period_to"></p>
