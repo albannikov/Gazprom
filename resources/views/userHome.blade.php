1d153be0384c067f43c179eb68f4fe7e8576d14a
albannikov <104481138+albannikov@users.noreply.github.com>
2022-12-03 16:06:54 +0500

    stylemenu

--- resources/views/userHome.blade.php
+++ resources/views/userHome.blade.php
@@ -12,19 +12,19 @@
 
  
     </head>
-    <header class="p-3 menu text-white">
+    <header class="p-3 menu">
     <div class="container">
       <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
-        <a href="/" class="d-flex align-items-center mb-2 mb-lg-0 text-white text-decoration-none">
+        <a href="/" class="d-flex align-items-center mb-2 mb-lg-0 text-decoration-none">
           <img src="/img/logo.png" class="cabinet-logo">
         </a>
 
         <ul class="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
           <li><a href="/home" class="nav-link px-2 text-secondary">Главная</a></li>
-          <li><a href="/car" class="nav-link px-2 text-white">Добавить машину</a></li>
-          <li><a href="/application" class="nav-link px-2 text-white">Добавить заявку</a></li>
-          <li><a href="#" class="nav-link px-2 text-white">FAQs</a></li>
-          <li><a href="#" class="nav-link px-2 text-white">About</a></li>
+          <li><a href="/car" class="nav-link px-2">Добавить машину</a></li>
+          <li><a href="/application" class="nav-link px-2">Добавить заявку</a></li>
+          <li><a href="#" class="nav-link px-2">FAQs</a></li>
+          <li><a href="#" class="nav-link px-2">About</a></li>
         </ul>
 
     
