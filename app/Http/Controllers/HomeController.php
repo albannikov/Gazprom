c4fe63887d111ea2caec0cbe9e34354b147dd994
audiowave33 <martinenkova@admrad.ru>
2022-12-03 11:42:12 +0300

    Страницы авторизации

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\User;
 
 class HomeController extends Controller
 {
@@ -21,8 +22,22 @@ class HomeController extends Controller
      *
      * @return \Illuminate\Contracts\Support\Renderable
      */
-    public function index()
+    public function index2(Request $request)
     {
-        return view('home');
+       
+        return view('adminHome');
+        
+        
     }
+
+    public function index(Request $request)
+    {
+       
+        
+        return view('userHome');
+        
+        
+    }
+
+    
 }
