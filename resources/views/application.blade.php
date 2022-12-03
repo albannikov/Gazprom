91deaefb9815bc5966ff0ab863ddaead4491f21e
albannikov <104481138+albannikov@users.noreply.github.com>
2022-12-03 22:32:51 +0500

    add form

--- resources/views/application.blade.php
+++ resources/views/application.blade.php
@@ -79,8 +79,12 @@
      <p>Точка А<input type="text" name="point_a"></p>
      <p>Точка Б<input type="text" name="point_b"></p>
      <p>Срок действия с<input type="date" name="period_from">по<input type="date" name="period_to"></p>
-     <button>Отправить</button>
+     <button class="btn btn-primary">Отправить</button>
 </form>
+
+
+
+
 @foreach($applications as $application)
     <p>Номер машины {{ $application->state_number }}</p>
     <p>Точка А {{ $application->point_a }}</p>
