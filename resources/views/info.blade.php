db42b0f664ab4c567f53b72f1343de63517594c2
audiowave33 <martinenkova@admrad.ru>
2022-12-04 02:07:51 +0300

    обновил информацию в ссылках

--- resources/views/info.blade.php
+++ resources/views/info.blade.php
@@ -8,7 +8,15 @@
     <p>Год выпуска: {{ $text->year_of_release }}</p>
     <p>Код ТС: {{ $text->code_ts }}</p>
     <p>Вид транспортной услуги: {{ $text->type_of_transport_service }}</p>
-    <p>Вид ТС: {{ $text->code_ts }}</p>
+    <p>Вид ТС: {{ $text->view_ts }}</p>
+    <p>Тип ТС: {{ $text->type_ts }}</p>
+    <p>Значение характеристики типа ТС: {{ $text->the_value_of_the_vehicle_type_characteristic }}</p>
+    <p>Страна производства: {{ $text->country_of_manufacture }}</p>
+    <p>Вид топлива: {{ $text->type_of_fuel }}</p>
+    <p>Поставщик ТУ: {{ $text->supplier	 }}</p>
+    <p>Субподряд: {{ $text->subcontracting }}</p>
+    <p>Основание владения: {{ $text->the_basis_of_ownership }}</p>
+    <p>СМА: {{ $text->sma }}</p>
     {!! QrCode::size(300)->generate('http://gp.admrad.ru/qr-code/' . $text->qr_code ) !!}
     <img src="/img/blank.jpeg" width="400" height="600">
 @endforeach
