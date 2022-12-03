5a8762e36f014f80e8259e106ab3aa32e8ba7604
audiowave33 <martinenkova@admrad.ru>
2022-12-04 01:54:02 +0300

    Добавил выводимые данные в ссылке

--- resources/views/info.blade.php
+++ resources/views/info.blade.php
@@ -1,7 +1,14 @@
-<p>Номер машины: {{ $info->state_number }}<p>
-<p>Точка А: {{ $info->point_a }}<p>
-<p>Точка Б: {{ $info->point_b }}<p>
-<p>С {{ $info->period_from }}<p>
-<p>По {{ $info->period_to }}<p>
-{!! QrCode::size(300)->generate('http://road.local/qr-code/' . $info->qr_code ) !!}
-<img src="/img/blank.jpeg" width="400" height="600">
+@foreach ($info as $text)
+    <p>Номер машины: {{ $text->state_number }}<p>
+    <p>Точка А: {{ $text->point_a }}<p>
+    <p>Точка Б: {{ $text->point_b }}<p>
+    <p>С {{ $text->period_from }}<p>
+    <p>По {{ $text->period_to }}<p>
+    <p>Модель ТС: {{ $text->model }}</p>
+    <p>Год выпуска: {{ $text->year_of_release }}</p>
+    <p>Код ТС: {{ $text->code_ts }}</p>
+    <p>Вид транспортной услуги: {{ $text->type_of_transport_service }}</p>
+    <p>Вид ТС: {{ $text->code_ts }}</p>
+    {!! QrCode::size(300)->generate('http://gp.admrad.ru/qr-code/' . $text->qr_code ) !!}
+    <img src="/img/blank.jpeg" width="400" height="600">
+@endforeach
