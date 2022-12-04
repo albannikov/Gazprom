620663ee282c845b5c0aa76d02f120ccc27cb916
albannikov <104481138+albannikov@users.noreply.github.com>
2022-12-04 09:55:39 +0500

    complete

--- resources/views/pass.blade.php
+++ resources/views/pass.blade.php
@@ -85,6 +85,8 @@
       <th scope="col">Действителен с</th>
       <th scope="col">Действителен по</th>
       <th scope="col">Уникальный код</th>
+      <th scope="col">telegram</th>
+      <th scope="col">VK</th>
       <!-- <th scope="col">QR-код</th> -->
 
      
@@ -96,9 +98,18 @@
       <td>{{ $pass->point_b }}</td>
       <td>{{ $pass->period_from }}</td>
       <td> {{ $pass->period_to }}</td>   
-      <td><a href="/qr-code/{{ $pass->qr_code }}" class="link">Уникальный код</a></td>
-     
+      <td><a href="/qr-code/{{ $pass->qr_code }}" >Уникальный код</a></td>
+      <td><a class="btn_telegram_share" href="https://telegram.me/share/url?url=https://gp.admrad.ru/qr-code/{{ $pass->qr_code }}" target="_blank">
+        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-telegram" viewBox="0 0 16 16">
+         <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM8.287 5.906c-.778.324-2.334.994-4.666 2.01-.378.15-.577.298-.595.442-.03.243.275.339.69.47l.175.055c.408.133.958.288 1.243.294.26.006.549-.1.868-.32 2.179-1.471 3.304-2.214 3.374-2.23.05-.012.12-.026.166.016.047.041.042.12.037.141-.03.129-1.227 1.241-1.846 1.817-.193.18-.33.307-.358.336a8.154 8.154 0 0 1-.188.186c-.38.366-.664.64.015 1.088.327.216.589.393.85.571.284.194.568.387.936.629.093.06.183.125.27.187.331.236.63.448.997.414.214-.02.435-.22.547-.82.265-1.417.786-4.486.906-5.751a1.426 1.426 0 0 0-.013-.315.337.337 0 0 0-.114-.217.526.526 0 0 0-.31-.093c-.3.005-.763.166-2.984 1.09z"/>
+          </svg>
+          Поделиться в телеграмме</a></td>
+      <td><a href="https://vk.com/share.php?url=http://https://gp.admrad.ru/qr-code/{{ $pass->qr_code }}" target="_blank"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-share-fill" viewBox="0 0 16 16">
+            <path d="M11 2.5a2.5 2.5 0 1 1 .603 1.628l-6.718 3.12a2.499 2.499 0 0 1 0 1.504l6.718 3.12a2.5 2.5 0 1 1-.488.876l-6.718-3.12a2.5 2.5 0 1 1 0-3.256l6.718-3.12A2.5 2.5 0 0 1 11 2.5z"/>
+          </svg> 
+        Поделиться ВКонтакте</a></td>
       <!-- <td>{!! QrCode::size(180)->generate('http://gp.admrad.ru/qr-code/' . $pass->qr_code ) !!}</td> -->
+      
     </tr>   
     @endforeach
   </tbody>
